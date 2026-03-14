f4171ed1f07fa40363433b076f20b2a471cbc99c
ZiyadCh <chetoziyad@gmail.com>
2026-03-14 22:05:51 +0000

    added website favicon

--- app/Models/Absence.php
+++ app/Models/Absence.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Absence extends Model
 {
+    protected $fillable = [
+        'date',
+        'justified',
+    ];
     public function students()
     {
         $this->belongsTo(Student::class);
