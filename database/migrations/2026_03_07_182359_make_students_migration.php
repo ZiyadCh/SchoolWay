9a9f32fa6e395ed6449b0d7a1440a7431a268195
ZiyadCh <chetoziyad@gmail.com>
2026-03-18 15:07:48 +0000

    modified code column into a string

--- database/migrations/2026_03_07_182359_make_students_migration.php
+++ database/migrations/2026_03_07_182359_make_students_migration.php
@@ -14,7 +14,7 @@ return new class extends Migration {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->integer('code');
+            $table->string('code');
             $table->float('note_final');
             $table->timestamps();
         });
