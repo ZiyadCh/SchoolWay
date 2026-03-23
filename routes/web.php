6b15f8e3a4dac25a4ad94f9af4a5e5260fb665f3
ZiyadCh <chetoziyad@gmail.com>
2026-03-23 18:50:42 +0100

    added api route

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,3 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-////////////////////
-//////////////////////////
-//Directeur
-Route::get('/students/profile', function (Request $request) {
-    $currentTab = $request->query('tab', 'notes');
-
-    return view('students.profile', [
-        'tab' => $currentTab,
-    ]);
-});
