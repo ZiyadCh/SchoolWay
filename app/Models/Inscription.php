288813dfdf0c867347aea1c897cf01c95c1be770
ZiyadCh <chetoziyad@gmail.com>
2026-03-28 17:54:23 +0100

    added the inscription orm relations and fillables

--- app/Models/Inscription.php
+++ app/Models/Inscription.php
@@ -3,8 +3,47 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Inscription extends Model
 {
-    //
+    protected $fillable = [
+        'student_id',
+        'school_class_id',
+        'year_id',
+        'note_final',
+        'statut',
+    ];
+
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function schoolClass(): BelongsTo
+    {
+        return $this->belongsTo(SchoolClass::class, 'school_class_id');
+    }
+
+    public function academicYear(): BelongsTo
+    {
+        return $this->belongsTo(AcademicYear::class, 'year_id');
+    }
+
+    public function exams(): HasMany
+    {
+        return $this->hasMany(Exam::class);
+    }
+
+    public function absences(): HasMany
+    {
+        return $this->hasMany(Absence::class);
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
 }
