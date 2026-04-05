9f4f0fff0eebef90507e113b58efec5626ec7d0f
ZiyadCh <chetoziyad@gmail.com>
2026-04-05 19:04:47 +0100

    gave the email a proper format , and gave the web login route the name login-web

--- resources/views/mail/send-password-to-user.blade.php
+++ resources/views/mail/send-password-to-user.blade.php
@@ -1,13 +1,29 @@
 <x-mail::message>
-Bienvenue sur SchoolWay. Voici vos identifiants pour vous connecter à votre espace :
+# Bienvenue sur School Way !
 
-- **Email :** {{ $user->email }}
-- **Mot de passe :** {{ $password }}
+Bonjour **{{ $user->nom }} {{ $user->prenom }}**,
 
-<x-mail::button :url="config('app.name').'/login'">
-Button Text
+Votre compte a été créé avec succès. Vous pouvez désormais accéder à votre espace personnel pour suivre vos cours, vos absences et vos notes.
+
+## Vos identifiants de connexion
+
+Pour vous connecter, utilisez les informations suivantes :
+
+<x-mail::panel>
+**Email :** `{{ $user->email }}`
+
+**Mot de passe :** `{{ $password }}`
+</x-mail::panel>
+
+<x-mail::button :url="route('login-page')">
+Accéder à mon espace
 </x-mail::button>
 
-Thanks,<br>
-{{ config('app.name') }}
+ **Conseil de sécurité :** Nous vous recommandons de modifier ce mot de passe dès votre première connexion depuis les paramètres de votre profil.
+
+Si vous rencontrez des difficultés pour vous connecter, n'hésitez pas à contacter l'administration de l'établissement.
+
+À très bientôt,
+
+L'équipe **{{config('app.name')}}**
 </x-mail::message>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
     return view('auth.login');
-});
+})->name('login-page');
 
 Route::get('students', function () {
     return view('students.list');
