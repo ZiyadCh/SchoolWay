8fa42d1d0203206ad67ba9c2a6cb9d6cd8804d76
ZiyadCh <chetoziyad@gmail.com>
2026-04-07 15:01:07 +0100

    changed golbal font to ubuntu and modified in absence view

--- resources/views/students/absences.blade.php
+++ resources/views/students/absences.blade.php
@@ -7,22 +7,22 @@
     <div class="max-w-5xl mx-auto space-y-8">
 
         <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <div class="bg-gray-900 border border-gray-800 p-6 rounded-2xl shadow-xl flex flex-col justify-between min-h-[120px]">
-                <p class="text-[10px] font-black text-gray-500 uppercase tracking-[0.2em]">Total Absences</p>
-                <p class="text-5xl font-black text-white tracking-tighter">12</p>
+            <div class="bg-gray-900 border border-gray-800 p-6 rounded-2xl shadow-xl flex flex-col justify-between items-center min-h-30">
+                <p class="text-l text-gray-500">Total Absences</p>
+                <p class="text-5xl text-white">12</p>
             </div>
 
-            <div class="bg-gray-900 border border-gray-800 p-6 rounded-2xl shadow-xl flex flex-col justify-between min-h-[120px]">
-                <p class="text-[10px] font-black text-gray-500 uppercase tracking-[0.2em]">Justifiées</p>
+            <div class="bg-gray-900 border border-gray-800 p-6 rounded-2xl shadow-xl flex flex-col justify-between items-center  min-h-30">
+                <p class="text-l text-gray-500">Justifiées</p>
                 <div class="flex items-baseline gap-2">
-                    <p class="text-5xl font-black text-emerald-500 tracking-tighter">08</p>
+                    <p class="text-5xl text-emerald-500">08</p>
                 </div>
             </div>
 
-            <div class="bg-gray-900 border border-gray-800 p-6 rounded-2xl shadow-xl flex flex-col justify-between min-h-[120px]">
-                <p class="text-[10px] font-black text-gray-500 uppercase tracking-[0.2em]">Non Justifiées</p>
+            <div class="bg-gray-900 border border-gray-800 p-6 rounded-2xl shadow-xl flex flex-col justify-center items-center min-h-30">
+                <p class="text-l text-gray-500">Non Justifiées</p>
                 <div class="flex items-baseline gap-2">
-                    <p class="text-5xl font-black text-red-500 tracking-tighter">04</p>
+                    <p class="text-5xl text-red-500">04</p>
                 </div>
             </div>
         </div>
@@ -32,45 +32,39 @@
                 <table class="w-full text-left border-collapse">
                     <thead>
                         <tr class="bg-gray-950/50 border-b border-gray-800">
-                            <th class="p-6 text-[10px] font-black uppercase tracking-widest text-gray-500">Date de l'absence</th>
-                            <th class="p-6 text-[10px] font-black uppercase tracking-widest text-gray-500 text-right">Statut Justification</th>
+                            <th class="p-6 text-[10px] text-gray-500">Date de l'absence</th>
+                            <th class="p-6 text-[10px] text-gray-500 text-right">Statut Justification</th>
                         </tr>
                     </thead>
                     <tbody class="divide-y divide-gray-800/50">
                         <tr class="hover:bg-gray-800 transition-colors">
-                            <td class="p-6">
-                                <div class="flex items-center gap-4">
-                                    <span class="font-bold text-gray-200 uppercase tracking-tight text-lg ">02 Avril 2026</span>
-                                </div>
+                            <td class="p-6 text-gray-200 text-lg">
+                                02 Avril 2026
                             </td>
                             <td class="p-6 text-right">
-                                <span class="px-4 py-1.5 bg-emerald-500/10 border border-emerald-500/20 rounded-full text-[9px] font-black uppercase tracking-widest text-emerald-500">
+                                <span class="px-4 py-1.5 bg-emerald-500/10 border border-emerald-500/20 rounded-full text-[9px] text-emerald-500">
                                     Justifiée
                                 </span>
                             </td>
                         </tr>
 
                         <tr class="hover:bg-gray-800 transition-colors">
-                            <td class="p-6">
-                                <div class="flex items-center gap-4">
-                                    <span class="font-bold text-gray-200 uppercase tracking-tight text-lg ">28 Mars 2026</span>
-                                </div>
+                            <td class="p-6 text-gray-200 text-lg">
+                                28 Mars 2026
                             </td>
                             <td class="p-6 text-right">
-                                <span class="px-4 py-1.5 bg-red-500/10 border border-amber-500/20 rounded-full text-[9px] font-black uppercase tracking-widest text-amber-500">
+                                <span class="px-4 py-1.5 bg-red-500/10 border border-amber-500/20 rounded-full text-[9px] text-amber-500">
                                     Non Justifiée
                                 </span>
                             </td>
                         </tr>
 
                         <tr class="hover:bg-gray-800 transition-colors">
-                            <td class="p-6">
-                                <div class="flex items-center gap-4">
-                                    <span class="font-bold text-gray-200 uppercase tracking-tight text-lg ">15 Mars 2026</span>
-                                </div>
+                            <td class="p-6 text-gray-200 text-lg">
+                                15 Mars 2026
                             </td>
                             <td class="p-6 text-right">
-                                <span class="px-4 py-1.5 bg-emerald-500/10 border border-emerald-500/20 rounded-full text-[9px] font-black uppercase tracking-widest text-emerald-500">
+                                <span class="px-4 py-1.5 bg-emerald-500/10 border border-emerald-500/20 rounded-full text-[9px] text-emerald-500">
                                     Justifiée
                                 </span>
                             </td>
